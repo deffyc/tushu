2539697b2ef0f88c2162bba05d7009bfc5ade937
deffyc <deffyc@139.com>
2023-05-15 16:51:32 +0800

    update

--- src/config/ajax.js
+++ src/config/ajax.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 import jsonp from 'jsonp'
-import sha1 from '../utils/sha1'
+// import sha1 from '../utils/sha1'
 
 export default {
   cloudapi: (url, method, data, callback) => {
-    let AppId = 'A6054211052206'
-    let AppKey = '5305A293-A078-235A-937E-92E35B768FD3'
-    let now = Date.now()
-    let appKey = ''//sha1.SHA1(AppId + 'UZ' + AppKey + 'UZ' + now) + '.' + now
-    let headers = {}//{'X-APICloud-AppId': AppId, 'X-APICloud-AppKey': appKey}
+    // let AppId = 'A6054211052206'
+    // let AppKey = '5305A293-A078-235A-937E-92E35B768FD3'
+    // let now = Date.now()
+    // let appKey = '' // sha1.SHA1(AppId + 'UZ' + AppKey + 'UZ' + now) + '.' + now
+    let headers = {} // {'X-APICloud-AppId': AppId, 'X-APICloud-AppKey': appKey}
     switch (method) {
       case 'GET':
-        //axios.get(url + encodeURIComponent(JSON.stringify(data)), {headers: headers})
-        axios.get(url + data, {headers: headers})
+        // axios.get(url + encodeURIComponent(JSON.stringify(data)), {headers: headers})
+        axios.get(url + data, { headers: headers })
           .then(function (response) {
             //  console.log(response)
-            callback(response)
+            callback(response['records'])
           })
           .catch(function (error) {
             console.log(error)
@@ -23,7 +23,7 @@ export default {
           })
         break
       case 'PUT':
-        axios.put(url, data, {headers: headers})
+        axios.put(url, data, { headers: headers })
           .then(function (response) {
             callback(response.data)
           })
--- src/services/search.js
+++ src/services/search.js
@@ -1,52 +1,52 @@
 import ajax from '../config/ajax'
-import tools from '../utils/tools'
+// import tools from '../utils/tools'
 const pageSize = 25
 export default {
   pageSize: pageSize,
   getBooks: (method, key, fields, type, pageno, callback) => {
-    //let where = {}
+    // let where = {}
     let filtedic = fields.join()
-    let where=''
+    let where = ''
     switch (method) {
       case 'bookno':
-        where = "&filter=no,eq,"+key//{'no': tools.zhuanyi(key)}
+        where = '&filter=no,eq,' + key // {'no': tools.zhuanyi(key)}
         break
       default:
-        //where['or'] = []
+        // where['or'] = []
         for (let i = 0; i < type.length; i++) {
-          //let fieldJson = {}
-          //fieldJson[type[i]] = {'like': (type[i] === 'no' ? '^' : '') + key + '.*'}
-          //where['or'].push(fieldJson)
-          where=where+"&filter"+(i+1)+"=sw,"+key
+          // let fieldJson = {}
+          // fieldJson[type[i]] = {'like': (type[i] === 'no' ? '^' : '') + key + '.*'}
+          // where['or'].push(fieldJson)
+          where = where + '&filter' + (i + 1) + '=no,sw,' + key
         }
     }
     // let filtedic = {'fields': fields, 'where': where, 'limit': pageSize, 'skip': (pageno - 1) * pageSize, 'order': 'createtime DESC,no ASC'}
     // let url = 'https://d.apicloud.com/mcm/api/Book?filter='
-    
-    let url = 'https://ehh.ccsyue.com/api.php/records/cat?include='+filtedic+where+'&order=createtime desc&order=no&page='+pageno+','+pageSize
+
+    let url = 'https://ehh.ccsyue.com/api.php/records/cat?include=' + filtedic + where + '&order=createtime desc&order=no&page=' + pageno + ',' + pageSize
 
     let methodstr = 'GET'
     ajax.cloudapi(url, methodstr, '', callback)
   },
   getCategories: (method, key, fields, type, pageno, callback) => {
-    let filtedic= fields.join()
-    let where = ''//{}
+    let filtedic = fields.join()
+    let where = '' // {}
     switch (method) {
       case 'categoryno':
-        where = "&filter=pno,eq,"+key// {'pno': tools.zhuanyi(key)}
+        where = '&filter=pno,eq,' + key // {'pno': tools.zhuanyi(key)}
         break
       default:
-        //where['or'] = []
+        // where['or'] = []
         for (let i = 0; i < type.length; i++) {
           // let fieldJson = {}
           // fieldJson[type[i]] = {'like': '%' + key + '%'}
           // where['or'].push(fieldJson)
-          where=where+"&filter"+(i+1)+"=sw,"+key
+          where = where + '&filter' + (i + 1) + '=sw,' + key
         }
     }
-    //let filtedic = {'fields': fields, 'where': where, 'limit': pageSize, 'skip': (pageno - 1) * pageSize, 'order': 'sort_num ASC'}
-    //let url = 'https://d.apicloud.com/mcm/api/Category?filter='
-    let url = 'https://ehh.ccsyue.com/api.php/records/category?include='+filtedic+where+'&order=sort_num&page='+pageno+','+pageSize
+    // let filtedic = {'fields': fields, 'where': where, 'limit': pageSize, 'skip': (pageno - 1) * pageSize, 'order': 'sort_num ASC'}
+    // let url = 'https://d.apicloud.com/mcm/api/Category?filter='
+    let url = 'https://ehh.ccsyue.com/api.php/records/category?include=' + filtedic + where + '&order=sort_num&page=' + pageno + ',' + pageSize
     let methodstr = 'GET'
     ajax.cloudapi(url, methodstr, '', callback)
   },
@@ -54,7 +54,7 @@ export default {
     ajax.doubanapi(isbn, callback)
   },
   saveBook: (id, book, callback) => {
-    //let url = 'https://d.apicloud.com/mcm/api/Book/' + id
-    //ajax.cloudapi(url, 'PUT', book, callback)
+    // let url = 'https://d.apicloud.com/mcm/api/Book/' + id
+    // ajax.cloudapi(url, 'PUT', book, callback)
   }
 }
